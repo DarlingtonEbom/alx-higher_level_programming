6d957a33a8af535b17a5c9697085c17b495460dd
DarlingtonEbom <darlingtonebom@gmail.com>
2023-08-16 06:54:13 -0700

    copy matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+# 0-square_matrix_simple.py
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+    new_matrix = (x**2 for x in i(new_matrix[i]))
+    return (new_matrix[i])
